bbabaa1e1ee17de1b0938a6b6c00d00c1ee6bd4a
misho1997 <122465544+misho1997@users.noreply.github.com>
2023-02-04 17:34:38 +0400

    misho1997

---
+++ app.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('button');
+const screenDisplay = document.querySelector('.screen');
+
+
+
+let calculation = []
+let accumulativeCalculation
+
+function calculate(button) {
+    const value = button.textContent
+    if(value ==="clear") {
+        calculation = []
+        screenDisplay.textContent = '.'
+    } else if(value ==="=") {
+        console.log(accumulativeCalculation)
+    screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value)
+    accumulativeCalculation = calculation.join('')
+    screenDisplay.textContent = accumulativeCalculation
+   
+    }
+
+    
+}
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)));
